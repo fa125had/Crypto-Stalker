91bf4bc829b9ea688e1c12dd6fad2c62ce7c179f
farshad <far5had@outlook.com>
2023-07-05 12:01:02 +0200

    added header

--- src/views/header/headerView.js
+++ src/views/header/headerView.js
@@ -1,11 +1,61 @@
 export const renderHeader = () => {
-    const header = document.createElement("header");
-    // Design your header element
-    // Add any necessary content or styling
-  
-    const title = document.createElement("h1");
-    title.textContent = "My App Header";
-    header.appendChild(title);
-  
-    return header;
-  };
+  // Render Header
+  const header = document.createElement("header");
+  header.classList.add("header-container");
+  // Render Navbar
+  const navbar = document.createElement("nav");
+  navbar.classList.add("navbar");
+  // Render Logo
+  const logoContainer = document.createElement("div");
+  logoContainer.classList.add("logo-container");
+  const logo = document.createElement("img");
+  logo.setAttribute("src", "./assets/images/logo.jpg");
+  logo.classList.add("logo");
+  logoContainer.appendChild(logo);
+  // Render Navbar Links
+  const navLinks = document.createElement("ul");
+  navLinks.classList.add("nav-links");
+  navLinks.setAttribute("id", "nav-links");
+  navLinks.setAttribute("role", "navigation");
+  // Render Home Link in Navbar
+  const homeLink = document.createElement("li");
+  homeLink.classList.add("nav-link");
+  homeLink.innerHTML = "Home";
+  navLinks.appendChild(homeLink);
+  // Render BTC Link in Navbar
+  const btcLink = document.createElement("li");
+  btcLink.classList.add("nav-link");
+  btcLink.innerHTML = "BTC";
+  navLinks.appendChild(btcLink);
+  // Render ETH Link in Navbar
+  const ethLink = document.createElement("li");
+  ethLink.classList.add("nav-link");
+  ethLink.innerHTML = "ETH";
+  navLinks.appendChild(ethLink);
+  // Render LTC Link in Navbar
+  const ltcLink = document.createElement("li");
+  ltcLink.classList.add("nav-link");
+  ltcLink.innerHTML = "LTC";
+  navLinks.appendChild(ltcLink);
+  // Render XRP Link in Navbar
+  const xrpLink = document.createElement("li");
+  xrpLink.classList.add("nav-link");
+  xrpLink.innerHTML = "XRP";
+  navLinks.appendChild(xrpLink);
+  // Render BCH Link in Navbar
+  const bchLink = document.createElement("li");
+  bchLink.classList.add("nav-link");
+  bchLink.innerHTML = "BCH";
+  navLinks.appendChild(bchLink);
+  // Render BNB Link in Navbar
+  const bnbLink = document.createElement("li");
+  bnbLink.classList.add("nav-link");
+  bnbLink.innerHTML = "BNB";
+  navLinks.appendChild(bnbLink);
+  // Add Navbar Links to Header
+  navbar.appendChild(logoContainer);
+  navbar.appendChild(navLinks);
+  header.appendChild(navbar);
+
+  return header;
+};
--- src/views/welcome/welcomeView.js
+++ src/views/welcome/welcomeView.js
@@ -1,27 +1,26 @@
 export const renderWelcome = async () => {
   return new Promise((resolve, reject) => {
     const ui = document.getElementById("user-interface");
-
+    // Render welcome page
     const welcomeContainer = document.createElement("div");
     const welcomeImage = document.createElement("img");
     const welcomeText = document.createElement("p");
-
+    // Set up welcome Page
     welcomeContainer.classList.add("welcome-container");
     welcomeContainer.setAttribute("id", "welcome-container");
-
+    // Set up welcome image
     welcomeImage.classList.add("welcome-image");
     welcomeImage.setAttribute("id", "welcome-image");
     welcomeImage.src = "./assets/images/welcome.jpg";
-
+    // Set up welcome text
     welcomeText.classList.add("welcome-text");
     welcomeText.setAttribute("id", "welcome-text");
     welcomeText.textContent = "Crypto Stalker";
-
+    // Add welcome page to UI
     welcomeContainer.appendChild(welcomeImage);
     welcomeContainer.appendChild(welcomeText);
-
     ui.appendChild(welcomeContainer);
-
+    // Remove welcome page after 4.5 seconds
     setTimeout(() => {
       welcomeContainer.style.display = "none";
       resolve(welcomeContainer);
